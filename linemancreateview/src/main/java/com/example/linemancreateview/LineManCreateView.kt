94b9fd1b215acd3c37af37cc82b52a0e0db91c83
Anwesh43 <anweshthecool0@gmail.com>
2021-02-08 02:38:16 +0530

    adding constants and libraries

--- linemancreateview/src/main/java/com/example/linemancreateview/LineManCreateView.kt
+++ linemancreateview/src/main/java/com/example/linemancreateview/LineManCreateView.kt
@@ -1,3 +1,29 @@
 package com.example.linemancreateview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
 
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#2196F3",
+    "#8BC34A",
+    "#009688",
+    "#673AB7"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val delay : Long = 20
+val rFactor : Float = 12.9f
+val handFactor : Float = 10.8f
+val legFactor : Float = 8.3f
