bfd3f05f2eaf3c86766c38762eb8d742f94ac492
Anwesh43 <anweshthecool0@gmail.com>
2021-02-08 03:41:04 +0530

    using right view in activity

--- app/src/main/java/com/example/linkedlinemancreateview/MainActivity.kt
+++ app/src/main/java/com/example/linkedlinemancreateview/MainActivity.kt
@@ -3,11 +3,12 @@ package com.example.linkedlinemancreateview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.example.linemancreateview.LineManCreateView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineManCreateView.create(this)
         fullScreen()
     }
 }
